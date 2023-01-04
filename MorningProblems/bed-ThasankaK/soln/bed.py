9ac33ef905296ed461108a6ae1c8f9808958b6d6
ThasankaK <thasanka.k@hotmail.com>
2023-01-03 19:24:18 -0700

    All CMPUT274 Assingments

---
+++ MorningProblems/bed-ThasankaK/soln/bed.py
@@ -0,0 +1,14 @@
+# read in the input
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+dist1 = abs(x2-x1)
+dist2 = abs(y2-y1)
+area = dist1*dist2
+print(area)
+
+
+# solve the problem
+
+
+
+# output the result
